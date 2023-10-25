9f8993c7fc749a70379bf9ff9deeb56c7ef82fa5
ntkien3 <ntkien3@tma.com.vn>
2023-10-25 14:35:20 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cart from './pages/Cart/Cart'
 import ProductList from './pages/ProductList/ProductList'
 import Home from './pages/Home/Home'
 const App = () => {
+  console.log('asd')
   return <Home />
 }
 
